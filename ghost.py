6de534c80df8eff40f721782215ad894c3c7282b
Devang <23aiml014@charusat.edu.in>
2025-05-18 11:26:56 +0530

    refactor: remove unused files and clean up the repository

--- ghost.py
+++
@@ -1,7 +0,0 @@
-import pywhatkit as pwk
-phone_number = "+9925456609"  # Replace with the recipient's phone number
-message = "Hello, this is a test message!"  # Replace with your message
-time_hour = 18  # Replace with the hour you want to send the message (24-hour format)
-time_minute = 10  # Replace with the minute you want to send the message
-# Send the message using pywhatkit
-pwk.sendwhatmsg(phone_number, message, time_hour, time_minute)
